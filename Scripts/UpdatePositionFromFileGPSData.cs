4c1287259e54e2ebf68cde378183cece932830ad
Reidar Devold Midtun <reidardmidtun@gmail.com>
2024-05-20 10:01:17 +0200

    Added UpdateDateFromExif and UpdatePositionFromFileGPSData

--- Scripts/UpdatePositionFromFileGPSData.cs
+++ Scripts/UpdatePositionFromFileGPSData.cs
@@ -5,25 +5,19 @@ using PhotoOrganizer;
 using PhotoOrganizer.ScriptController;
 
 namespace PhotoOrganizerScripts {
-    class SetDate : BaseScript {
-        enum Id { InputMode, Date };
-        private DateTime? _date;
+    class UpdatePositionFromFileGPSData : BaseScript {
+        enum Id { InputMode };
         private List<DB.Image>? _updatedImages;
 
-        public SetDate() {
+        public UpdatePositionFromFileGPSData() {
             InputVariables = new List<Parameter> {
                 new Parameter {
                     Id = Id.InputMode, ObjectType = typeof (InputImages_e), Label = "Input mode", 
                     DefaultValue = InputImages_e.CurrentFolderImages,
                     Description = "What images are to be processed", Mandatory = true
-                },
-                new Parameter {
-                    Id = Id.Date, ObjectType = typeof (DateTime), Label = "A date",
-                    DefaultValue = DateTime.Now, // Your default value
-                    Description = "The date to the images.", Mandatory = true
                 }
             };
-            Description = "Select which images to apply the process to. If the image date is not set, the date is set to the value given by the user.";
+            Description = "Select which images to apply the process to. If a gps position is found in the file meta data properties, the position of the image is updated.";
         }
 
         protected override IEnumerable<DB.Image> GetInputImages() {
@@ -32,14 +26,20 @@ namespace PhotoOrganizerScripts {
 
         protected override void PrepareExecution() {
             base.PrepareExecution(); // Caches the input images before starting the processing of images
-            _date = GetObject<DateTime>(Id.Date);
             _updatedImages = new List<DB.Image>();
         }
 
         protected override bool ProcessImage(DB.Image image) {
             var ok = true;
             try {
-                MyImageProcessing(image, _date);
+                var gpsLocation = DB.Image.GetGeoLocation(image.FileName);
+                if (gpsLocation != null) {
+                    if (image.Position == null || 
+                        gpsLocation.Latitude != image.Position.Latitude || gpsLocation.Longitude != image.Position.Longitude) {
+                        image.Position = gpsLocation;
+                        _updatedImages.Add(image);
+                    }
+                }
             }
             catch {
                 ok = false;
@@ -50,21 +50,9 @@ namespace PhotoOrganizerScripts {
         protected override void EndExecution() {
             if (_updatedImages != null && _updatedImages.Any()) {
                 DB.Update(_updatedImages);
-                LogView.AppendText($"Updated {_updatedImages.Count} with date {_date.Value.ToShortDateString()}.");
+                LogView.AppendText($"Updated {_updatedImages.Count} images.");
             } 
-        }
-
-        private void MyImageProcessing(DB.Image image, DateTime? date) {
-            if (date != null && _updatedImages != null && IsUpdateNeeded(image)) {
-                image.DateTime = date;
-                _updatedImages.Add(image);
-            }
-        }
-
-        // If the date is not set for the image, true is returned
-        private bool IsUpdateNeeded(DB.Image image) {
-            return image.DateTime == null; // Tailor to your need! 
-        }
+        }    
 
     }
 }
